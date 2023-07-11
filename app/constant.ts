e3bda0c4ae6945be13f2f10dcad9d8ca4d058d4a
kosmosr <kosmosr@hotmail.com>
2023-07-11 15:43:59 +0800

    小修改

--- app/constant.ts
+++ app/constant.ts
@@ -69,19 +69,7 @@ export const DEFAULT_MODELS = [
     available: true,
   },
   {
-    name: "gpt-4-0314",
-    available: true,
-  },
-  {
-    name: "gpt-4-0613",
-    available: true,
-  },
-  {
     name: "gpt-4-32k",
-    available: true,
-  },
-  {
-    name: "gpt-4-32k-0314",
     available: true,
   },
   {
@@ -90,14 +78,6 @@ export const DEFAULT_MODELS = [
   },
   {
     name: "gpt-3.5-turbo",
-    available: true,
-  },
-  {
-    name: "gpt-3.5-turbo-0301",
-    available: true,
-  },
-  {
-    name: "gpt-3.5-turbo-0613",
     available: true,
   },
   {
